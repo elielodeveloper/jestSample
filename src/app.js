be2acf4a189b74b281c7c4d74b56a862d5ee1e74
Eliel Roger <elielrogernic@gmail.com>
2019-05-02 20:22:33 -0300

    chore(setting up basic structure)

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+
+class AppController {
+  //Esta classe é responsável pela definição de Midlewares routes
+  //Alocação de portas e etc
+  constructor() {
+    this.express = express();
+
+    this.middlewares();
+    this.routes();
+  }
+
+  middlewares() {
+    this.express.use(express.json());
+  }
+
+  routes () {
+    this.express.use(require("./routes"));
+  }
+}
+
+module.exports = new AppController().express;
---
+++ src/server.js
@@ -0,0 +1,5 @@
+const app = require('./app');
+
+/*Arquivo responsável por configurações gerais do servidor*/ 
+
+app.listen(process.env.PORT || 3000);
