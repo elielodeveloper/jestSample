c239fff307a89c586e34010001c39e3dcd515c48
Eliel Roger <elielrogernic@gmail.com>
2019-05-03 08:32:17 -0300

    test(user-session)

--- __tests__/integration/session.test.js
+++ __tests__/integration/session.test.js
@@ -1,12 +1,29 @@
 const { User } = require("../../src/models");
 
 describe('Authentication', () => {
-  it('should receive JWT token when authenticaed with valid credentials', async () => {
+  // it('should receive JWT token when authenticaed with valid credentials', async () => {
+  //   const user = await User.create({
+  //     name: "Eliel",
+  //     email: "eliel@gmail.com",
+  //     password_hash: "123123123"
+  //   });
+  //   expect(user.email).toBe('eliel@gmail.com');
+  // });
+
+  it('should authenticate with valid credentials', async () => {
     const user = await User.create({
-      name: "Eliel",
-      email: "eliel@gmail.com",
-      password_hash: "123123123"
-    });
-    expect(user.email).toBe('eliel@gmail.com');
+      name: 'Eliel',
+      email: 'elielrogernic@gmail.com',
+      password_hash: '123123123'
+    })
+
+    const response = await request(app)
+      .post('/sessions')
+      .send({
+        email: user.email,
+        password: '123123123'
+      })
+    
+    expect(response.status).toBe(200);
   });
 })
